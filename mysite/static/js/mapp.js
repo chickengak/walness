5c3a98a7859619ba38ae7124c315d10c9d49a471
Jin <peyoumonephu@gmail.com>
2024-04-04 08:22:53 +0000

    Fix sidebar sorting

--- mysite/static/js/mapp.js
+++ mysite/static/js/mapp.js
@@ -1,5 +1,3 @@
-let globalSortBy = "Init";
-
 function initMap() {
   // Create the map.
   const curLocation = { lat: 36.8, lng: 127.1 };
@@ -70,7 +68,6 @@ function initMap() {
   function addPlaces(places, map, sortBy) {
     const placesList = document.getElementById("places");
     placesList.innerHTML = '';
-    const sortedPlaces = places.filter(place => place.rating).sort((a, b) => b.rating - a.rating);
 
     // 거리 정보를 함께 가지는 객체 배열로 변환
     const placesWithDistance = places.map(place => {
@@ -82,111 +79,76 @@ function initMap() {
         return { ...place, distance };
       }
       return null;
-    }).filter(place => place); // 유효한 데이터만 필터링
+    }).filter(place => place).sort((a, b) => a.distance - b.distance); // 유효한 데이터만 필터링 후, 거리순으로 정렬
 
-    // 거리순으로 정렬
-    placesWithDistance.sort((a, b) => a.distance - b.distance);
-
-    if (sortBy == 'distance') {
-      for (const place of placesWithDistance) {
-        if (place.geometry && place.geometry.location) {
-          const image = {
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(25, 25),
-          };
-  
-          const marker = new google.maps.Marker({
-            map,
-            icon: image,
-            title: place.name,
-            position: place.geometry.location,
-          });
-
-          const distanceInKm = (place.distance / 1000).toFixed(1); // 거리를 km 단위로 반올림(소수 첫째 자리까지)
-          
-          const p = document.createElement("div");
-          p.textContent = '★' + place.rating + '　' + `(${distanceInKm}km)`;
-
-          const li = document.createElement("li");
-          li.textContent = `${place.name}`;
-          li.classList.add("hospitalsidebar");
-          li.appendChild(p);
-          placesList.appendChild(li);
-          li.addEventListener("click", () => {
-            // 이전 팝업 닫기
-            closePreviousPopup();
-            // 중앙 이동
-            map.setCenter(place.geometry.location);
-            // 장소 이름 팝업 생성
-            const infoWindow = new google.maps.InfoWindow({
-              content: `<h5>${place.name}<h5>`
-            });
-            infoWindow.open(map, marker);
-            currentInfoWindow = infoWindow;
-
-            // li 태그들에서 active 클래스 제거하고, 현재 클릭한 li만 active 클래스 추가함. 그리고 css가 스타일을 추가한다.
-            document.querySelectorAll('.hospitalsidebar').forEach(item => {
-              item.classList.remove('active');
-            });
-            li.classList.add('active');
-          });
-        }
+    // 별점순으로 정렬
+    const placesWithRating = places.map(place => {
+      if (place.geometry && place.geometry.location) {
+        const distance = calculateDistance(
+          curLocation.lat, curLocation.lng,
+          place.geometry.location.lat(), place.geometry.location.lng()
+        );
+        return { ...place, distance };
       }
+      return null;
+    }).filter(place => place.rating).sort((a, b) => b.rating - a.rating);
+    
+
+    let sortedPlaces;
+    if (sortBy == 'distance') {
+      sortedPlaces = placesWithDistance;
     }
     else if (sortBy == 'rating') {
-      for (const place of sortedPlaces) {
-        if (place.geometry && place.geometry.location) {
-          const image = {
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(25, 25),
-          };
-  
-          const marker = new google.maps.Marker({
-            map,
-            icon: image,
-            title: place.name,
-            position: place.geometry.location,
-          });
-  
-          // appending rating in p tag
-          const p = document.createElement("div");
-          p.textContent = '★ ' + place.rating + '    ' ;
-  
-          const li = document.createElement("li");
-  
-          li.textContent = place.name;
-          li.classList.add("hospitalsidebar");
-          li.appendChild(p);
-          placesList.appendChild(li);
-          li.addEventListener("click", () => {
-            // 이전 팝업 닫기
-            closePreviousPopup();
-            // 중앙 이동
-            map.setCenter(place.geometry.location);
-            // 장소 이름 팝업 생성
-            const infoWindow = new google.maps.InfoWindow({
-              content: `<h5>${place.name}<h5>`
-            });
-            infoWindow.open(map, marker);
-            currentInfoWindow = infoWindow;
-
-            // li 태그들에서 active 클래스 제거하고, 현재 클릭한 li만 active 클래스 추가함. 그리고 css가 스타일을 추가한다.
-            document.querySelectorAll('.hospitalsidebar').forEach(item => {
-              item.classList.remove('active');
-            });
-            li.classList.add('active');
-          });
-        }
-      }
+      sortedPlaces = placesWithRating;
     }
 
+    for (const place of sortedPlaces) {
+      if (place.geometry && place.geometry.location) {
+        const image = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25),
+        };
 
+        const marker = new google.maps.Marker({
+          map,
+          icon: image,
+          title: place.name,
+          position: place.geometry.location,
+        });
+
+        const distanceInKm = (place.distance / 1000).toFixed(1); // 거리를 km 단위로 반올림(소수 첫째 자리까지)
+        
+        const p = document.createElement("div");
+        p.textContent = '★' + place.rating + '　' + `(${distanceInKm}km)`;
+
+        const li = document.createElement("li");
+        li.textContent = `${place.name}`;
+        li.classList.add("hospitalsidebar");
+        li.appendChild(p);
+        placesList.appendChild(li);
+        li.addEventListener("click", () => {
+          // 이전 팝업 닫기
+          closePreviousPopup();
+          // 중앙 이동
+          map.setCenter(place.geometry.location);
+          // 장소 이름 팝업 생성
+          const infoWindow = new google.maps.InfoWindow({
+            content: `<h5>${place.name}<h5>`
+          });
+          infoWindow.open(map, marker);
+          currentInfoWindow = infoWindow;
+
+          // li 태그들에서 active 클래스 제거하고, 현재 클릭한 li만 active 클래스 추가함. 그리고 css가 스타일을 추가한다.
+          document.querySelectorAll('.hospitalsidebar').forEach(item => {
+            item.classList.remove('active');
+          });
+          li.classList.add('active');
+        });
+      }
+    }
   }
 
   function calculateDistance(lat1, lng1, lat2, lng2) {
