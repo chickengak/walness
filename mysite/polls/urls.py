d42631eb96f97827c5c3a80a4d7247e627a49940
Jin <peyoumonephu@gmail.com>
2024-04-10 11:31:32 +0900

    Using the attached image, upload and then load it

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,8 +6,8 @@ from django.conf import settings
 urlpatterns = [
     path("", views.index, name="index"),
     path("left-sidebar/", views.left_sidebar_view, name="left_sidebar_view"),
-    path("right-sidebar/", views.right_sidebar_view, name="right_sidebar_view"),
-    path("no-sidebar/", views.no_sidebar_view, name="no_sidebar_view")
+    path("no-sidebar/", views.no_sidebar_view, name="no_sidebar_view"),
+    path("createform/", views.product_create, name="createform"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # media 경로 추가
 
